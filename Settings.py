82ed77b86bd553b61d325b49321019b766194a35
Nik <vayrikh.n@gmail.com>
2024-02-04 13:22:22 +0700

    Вернул значения переменных в исходное состояние

--- Settings.py
+++ Settings.py
@@ -1,11 +1,11 @@
 Number_Experiments = 1
-Tactic_Number = 0
+Tactic_Number = 1
 
-Number_Prisoners = 10
-Number_Try = 5
+Number_Prisoners = 100
+Number_Try = 50
 
 Print_In_Cmd = True
-Full_Report_In_Cmd = True
+Full_Report_In_Cmd = False
 
 Create_Text_File = False
 Full_Report_In_Text_File = False
