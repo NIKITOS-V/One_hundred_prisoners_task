2b3d3ae4bf07a2381c6165b0e8597e851198e28b
Nik <vayrikh.n@gmail.com>
2024-02-04 13:09:53 +0700

    Адоптировал функции для записи отчёта

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ from Settings import *
 from Room import Room
 from Prisoner import Prisoner
 from CheckInput import CheckInput
+from Report import Report
 
 
 class Experiment:
@@ -9,52 +10,55 @@ class Experiment:
     RandomNumbers = [number for number in range(1, Number_Prisoners+1)]
 
     def __init__(self):
-        self.CheckInput = CheckInput()
-
         self.NumberExperiments = Number_Experiments
-        self.ExperimentMode = Experiment_Mode
         self.NumberPrisoners = Number_Prisoners
-        self.NumberTry = Number_Try
-        self.PrintInCmd = Print_In_Cmd
-        self.CrateTextFile = Create_Text_File
-        self.ReportInCmd = Full_Report_In_Cmd
-        self.ReportInTextFile = Full_Report_In_Text_File
 
         self.GoodResults = 0
+        self.DoFullReport = Full_Report_In_Text_File or Full_Report_In_Cmd
 
         self.Room = Room(self.RandomNumbers)
-        self.Prisoner = Prisoner(self.RandomNumbers, self.NumberTry)
+        self.Prisoner = Prisoner(self.RandomNumbers, Number_Try, Tactic_Number, self.DoFullReport)
+        self.Report = Report()
 
         self.StartExperiment()
 
     def StartExperiment(self):
-        for ExperimentNumber in range(1, self.NumberExperiments+1):
-            print(f'\n\nНомер эксперимента: {ExperimentNumber}\n' )
+        if self.DoFullReport:
+            self.Experiment = self.ExperimentWithReport
+        else:
+            self.Experiment = self.ExperimentWithOutReport
 
-            self.Room.GeneraneField()
+        for ExperimentNumber in range(1, self.NumberExperiments+1):
+            self.Room.GenerateField()
             self.Experiment()
 
-        print('\n')
-        print(f'Шанс: {self.GoodResults / self.NumberExperiments * 100}%, '
-              f'Хороших исходов: {self.GoodResults}, '
-              f'Неудачных исходов {self.NumberExperiments - self.GoodResults}')
+        self.Report.SaveSimulationResult(self.GoodResults)
+        self.Report.PrintReport()
 
-    def Experiment(self):
-        if self.ExperimentMode == 0:
-            for PrisonerNumber in range(1, self.NumberPrisoners+1):
-                if not self.Prisoner.Choise_0(self.Room.Field, PrisonerNumber):
-                    print('\nЭксперимент завершён неудачно')
-                    return
-            self.GoodResults += 1
-            print('\nЭксперимент завершён удачно')
+    def ExperimentWithReport(self):
+        for PrisonerNumber in range(1, self.NumberPrisoners+1):
+            PrisonerResult = self.Prisoner.EnteringTheRoom(self.Room.Field, PrisonerNumber)
 
-        elif self.ExperimentMode == 1:
-            for PrisonerNumber in range(1, self.NumberPrisoners+1):
-                if not self.Prisoner.Choise_1(self.Room.Field, PrisonerNumber):
-                    print('\nЭксперимент завершён неудачно')
-                    return
-            self.GoodResults += 1
-            print('\nЭксперимент завершён удачно')
+            self.Report.SavePrisonerResult(PrisonerResult)
+
+            if not PrisonerResult[0]:
+                self.Report.SaveExperimentResult(0)
+
+                return
+
+        self.GoodResults += 1
+
+        self.Report.SaveExperimentResult(1)
+
+    def ExperimentWithOutReport(self):
+        for PrisonerNumber in range(1, self.NumberPrisoners+1):
+            PrisonerResult = self.Prisoner.EnteringTheRoom(self.Room.Field, PrisonerNumber)
+
+            if not PrisonerResult:
+                return
+
+        self.GoodResults += 1
 
 
+ChkInp = CheckInput()
 Exp = Experiment()
