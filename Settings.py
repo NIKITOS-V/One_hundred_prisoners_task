144eeb39f799a4c70ba483b9b4101e6d074fb03e
Nik <vayrikh.n@gmail.com>
2024-02-04 13:06:40 +0700

    Переименовал переменную ExperimentMode на Tactic_Number

--- Settings.py
+++ Settings.py
@@ -1,10 +1,11 @@
-Number_Experiments = 100
-Experiment_Mode = 1
+Number_Experiments = 1
+Tactic_Number = 0
 
-Number_Prisoners = 100
-Number_Try = 50
+Number_Prisoners = 10
+Number_Try = 5
 
-Print_In_Cmd = False
+Print_In_Cmd = True
+Full_Report_In_Cmd = True
+
 Create_Text_File = False
-Full_Report_In_Cmd = False
 Full_Report_In_Text_File = False
