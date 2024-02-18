d258ef895a1a14cfb10aa83321e204c026bbf288
Nik <vayrikh.n@gmail.com>
2024-02-18 21:44:10 +0700

    Расширил список проверок параметров эксперимента

--- CheckInput.py
+++ CheckInput.py
@@ -3,32 +3,48 @@ from Settings import *
 
 class CheckInput:
     def __init__(self):
-        self.CheckType()
-        self.CheckValue()
 
-    def CheckType(self):
-        try:
-            int(Number_Experiments)
-            int(Tactic_Number)
-            int(Number_Prisoners)
-            int(Number_Try)
-        except ValueError:
-            self.RaiseGenerate('Переменные Number_Experiments, Tactic_Number, Number_Prisoners, Number_Try'
-                               'должны иметь целочисленные значения')
+        def RaiseGenerate(Message):
+            raise ValueError(Message)
 
-    def CheckValue(self):
-        if type(Print_In_Cmd) != type(True):
-            self.RaiseGenerate('Переменная Print_In_Cmd должна иметь логический тип данных')
+        def BoolMessageGenerate(var):
+            return f'Переменная {var} должна иметь логический тип данных'
+
+        def IntMessageGenerate(var):
+            return f'Переменная {var} должна иметь целочисленное значение'
+
+        if type(Number_Experiments) != int:
+            RaiseGenerate(IntMessageGenerate('Number_Experiments'))
+
+        if type(Tactic_Number) != int:
+            RaiseGenerate(IntMessageGenerate('Tactic_Number'))
+
+        if type(Number_Prisoners) != int:
+            RaiseGenerate(IntMessageGenerate('Number_Prisoners'))
+
+        if type(Number_Try) != int:
+            RaiseGenerate(IntMessageGenerate('Number_Try'))
+
+        if Number_Experiments <= 0:
+            RaiseGenerate('Значение переменной Number_Experiments должно быть больше нуля')
+
         if Tactic_Number not in [0, 1]:
-            self.RaiseGenerate('Переменная Experiment_Mode может принимать только значения: 0, 1')
-        if type(Create_Text_File) != type(True):
-            self.RaiseGenerate('Переменная Create_Text_File должна иметь логический тип данных')
-        if type(Full_Report_In_Cmd) != type(True):
-            self.RaiseGenerate('Переменная Full_Report_In_Cmd должна иметь логический тип данных')
-        if type(Full_Report_In_Text_File) != type(True):
-            self.RaiseGenerate('Переменная Full_Report_In_Text_File должна иметь логический тип данных')
-        if Number_Try > Number_Prisoners:
-            self.RaiseGenerate('Число попыток не должно быть больше числа заключённых')
+            RaiseGenerate('Переменная Experiment_Mode может принимать только значения: 0, 1')
 
-    def RaiseGenerate(self, Message):
-        raise ValueError(Message)
+        if Number_Try > Number_Prisoners:
+            RaiseGenerate('Значение переменной Number_Try не должно быть больше значения переменной Number_Prisoners')
+
+        elif Number_Try < 0:
+            RaiseGenerate('Значение переменной Number_Try должно быть неотрицательным')
+
+        if type(Print_In_Cmd) != bool:
+            RaiseGenerate(BoolMessageGenerate('Print_In_Cmd'))
+
+        if type(Create_Text_File) != bool:
+            RaiseGenerate(BoolMessageGenerate('Create_Text_File'))
+
+        if type(Full_Report_In_Cmd) != bool:
+            RaiseGenerate(BoolMessageGenerate('Full_Report_In_Cmd'))
+
+        if type(Full_Report_In_Text_File) != bool:
+            RaiseGenerate(BoolMessageGenerate('Full_Report_In_Text_File'))
