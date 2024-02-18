ca3c07418c9d0e877a7a206d1b886c646bb61f51
Nik <vayrikh.n@gmail.com>
2024-02-18 21:48:13 +0700

    Изменил расположение текущих переменных, добавил переменную, определяющую кол-во ящиков в комнате (на момент коммита не используется)

--- Settings.py
+++ Settings.py
@@ -1,8 +1,11 @@
-Number_Experiments = 1
-Tactic_Number = 1
+Number_Experiments = 10
+
 
 Number_Prisoners = 100
+Tactic_Number = 0
 Number_Try = 50
+
+#  Number_Box = Number_Prisoners на данный момент не используется
 
 Print_In_Cmd = True
 Full_Report_In_Cmd = False
